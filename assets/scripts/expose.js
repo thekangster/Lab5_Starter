b6cd85d8c0d2870e71d01d5582c12a3d61ec5f85
Kevin Kang <kevinkang513@gmail.com>
2023-05-06 16:56:45 -0700

    expose

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,43 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+    const select = document.getElementById('horn-select');
+    const children = Array.from(expose.children);
+    const hornimg = children.find(elem => elem.nodeName === 'IMG');
+    const hornaud = children.find(elem => elem.nodeName === 'AUDIO');
+    const button = children.find(elem => elem.nodeName === 'BUTTON');
+    const volcont = document.getElementById('volume-controls')
+    const volicon = Array.from(volcont.children).find(elem => elem.nodeName === 'IMG')
+    const volume = document.getElementById('volume');
+    const jsConfetti = new JSConfetti()
+
+    select.addEventListener('change', () => {
+        hornimg.src = `./assets/images/${select.value}.svg`;
+        hornaud.src = `./assets/audio/${select.value}.mp3`;
+    })
+
+    button.addEventListener('click', () => {
+        hornaud.play();
+        if (select.value === 'party-horn') {
+            jsConfetti.addConfetti();
+        }
+    })
+
+    volume.addEventListener('input', () => {
+        console.log(volume.value);
+        volicon.src = `assets/icons/volume-level-${getVolume(volume.value)}.svg`
+        console.log(volicon.src);
+        hornaud.volume = volume.value / 100;
+    })
+
+    function getVolume(value) {
+        if (value == 0) {
+            return 0;
+        } else if (value < 33) {
+            return 1;
+        } else if (value < 67) {
+            return 2;
+        }
+        return 3;
+    }
 }

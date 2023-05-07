6bff7c7488937652de53874274e3935c07293b61
Kevin Kang <kevinkang513@gmail.com>
2023-05-07 13:43:10 -0700

    explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,37 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const explore = document.getElementById('explore');
+  const children = Array.from(explore.children);
+  const synth = window.speechSynthesis;
+  const select = document.getElementById('voice-select');
+  let voices = [];
+
+  window.addEventListener('load', () => {
+    voices = synth.getVoices();
+
+    for (let i = 0; i < voices.length; i++) {
+      let option = document.createElement('option');
+      option.textContent = `${voices[i].name} + (${voices[i].lang})`;
+      option.value = i;
+      select.appendChild(option);
+    }
+  })
+
+  const button = children.find(elm => elm.nodeName === 'BUTTON');
+  button.addEventListener('click', () => {
+    const text = document.getElementById('text-to-speak');
+    const utterance = new SpeechSynthesisUtterance(text.value);
+
+    const img = children.find(elm => elm.nodeName === 'IMG');
+    utterance.addEventListener('end', () => {
+      img.alt = 'smiling face';
+      img.src = 'assets/images/smiling.png';
+    })
+
+    utterance.voice = voices[select.value];
+    img.alt = 'speaking face';
+    img.src = 'assets/images/smiling-open.png';
+    synth.speak(utterance);
+  });
 }
